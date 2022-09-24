2087e4e463967d3a7e5efad96a35adcc8664b896
Vlad <molvlad@frtk.ru>
2022-09-24 20:29:42 +0300

    Tasks 2 and 3 completed

--- WaveformAnalyzer.m
+++ WaveformAnalyzer.m
@@ -8,13 +8,13 @@ classdef WaveformAnalyzer < handle
     %
     % Входные данные:
     %
-    % waveformSource - массив содержащий отчеты baseband сигнала во временной области на выходе OFDM модулятора
+    % waveformSamples - массив содержащий отчеты baseband сигнала во временной области на выходе OFDM модулятора
     %
     % waveformInfo - структура с параметрами OFDM модулятора и пейлоуда:
     %       Nfft               - кол-во спектрально-временных отчетов дискретного преобразования Фурье
-    %       SampleRate         - частота семплирования [Гц]
-    %       CyclicPrefixLengths/SymbolLengths - длины циклического преффикса и OFDM символов [кол-во временных отчетов]
-    %       SymbolsCount       - кол-во символов на слот радиокадра
+    %       sampleRate         - частота семплирования [Гц]
+    %       cyclicPrefixLengths/SymbolLengths - длины циклического преффикса и OFDM символов [кол-во временных отчетов]
+    %       symbolsCount       - кол-во символов на слот радиокадра
     %       subCarriersCount   - кол-во поднесущих
     %       payloadSymbols     - информационные символы
     %       payloadSymbolsIdxs - индексы ресурсных элементов отведенные для передачи payloadSymbols
@@ -23,10 +23,10 @@ classdef WaveformAnalyzer < handle
     %
     %       rmsEvm            - среднеквадратичное значение модуля вектора ошибки
     %       waveformMeanPower - среднеквадратичное значение мощности сигнала
-    %       channelBandwidth  - ширина полосы канала
-    %       noiseMeanPower    - среднеквадратичное значение мощности шума
+    %       channelBandwidth  - ширина полосы канала в дБ
+    %       noiseMeanPower    - среднеквадратичное значение мощности шума в разах
     %       modulationType    - тип модуляционной схемы
-    %       waveformDuration  - длина анализируемого сигнала
+    %       waveformDuration  - длина анализируемого сигнала в секундах
     %
 
     properties
@@ -38,22 +38,74 @@ classdef WaveformAnalyzer < handle
         waveformDuration
     end
 
+    properties (Access = private)
+        waveformSource        
+        Nfft
+        sampleRate
+        cyclicPrefixLengths
+        symbolLengths
+        symbolsCount
+        subCarriersCount
+        payloadSymbols
+        payloadSymbolsIdxs
+    end
+    
     methods
-        function this = WaveformAnalyzer()
+        function this = WaveformAnalyzer(waveformSource, waveformInfo)
             % Конструктор класса. Чтение waveform-ы во временной области и структуры с информацией
             % необходимой для дальнейшей обработки данных и заполнения полей класса
+            
+            % Сохранить отсчеты сигнала
+            this.waveformSource = waveformSource;
+            
+            % Сохранить параметры, считанные из структуры
+            this.Nfft = waveformInfo.Nfft;
+            this.sampleRate = waveformInfo.SampleRate;
+            this.cyclicPrefixLengths = waveformInfo.CyclicPrefixLengths;
+            this.symbolLengths = waveformInfo.SymbolLengths;
+            this.symbolsCount = waveformInfo.symbolsCount;
+            this.subCarriersCount = waveformInfo.subCarriersCount;
+            this.payloadSymbols = waveformInfo.payloadSymbols;
+            this.payloadSymbolsIdxs = waveformInfo.payloadSymbolsIdxs;
         end
 
         function calcWaveformParameters(this)
-
+            % Рассчитать среднюю мощность сигнала
+            this.waveformMeanPower = mean(abs(this.waveformSource).^2);
+            
+            % Рассчитать полосу сигнала
+            this.channelBandwidth = this.sampleRate;
+            
+            % Распознать тип модуляции
+            switch length(unique(this.payloadSymbols))
+                case 64
+                    this.modulationType = "QAM-64";
+                otherwise
+                    error("Неизвестная модуляция");
+            end
+            
+            % Рассчитать длительность сигнала
+            this.waveformDuration = length(this.waveformSource) / this.sampleRate;
         end
 
         function plotPowerSpectrumDensity(this)
-
+            % Получить частоты сигнала
+            waveformLength = length(this.waveformSource);
+            frequencies = (-waveformLength/2:waveformLength/2-1)*(this.sampleRate/waveformLength);
+            
+            % Построить спектральную плотность мощности
+            figure;
+            plot(frequencies, pow2db(abs(fftshift(fft(this.waveformSource))).^2));
+            xlabel("Frequency, Hz");
+            ylabel("Power, dB");
+            grid on;
+            title("Power spectrum density");
         end
 
         function plotPayloadConstellation(this)
-
+            % Построить созвездие
+            scatterplot(this.payloadSymbols);
+            title("Constellation");
         end
 
         function calcEvmPerformance(this)
--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,43 @@
 % Скрипт для запуска waveformAnalyxer
+
+close all
+clear all
+clc
+
+% Директория с файлами
+sourceDirectory = "waveform";
+
+% Название файла с описанием сигнала
+waveformInfoName = "waveformInfo.mat";
+
+% Название файла с отсчетами сигнала
+waveformSourceName = "waveformSource.mat";
+
+% Загрузить описание сигнала
+f = load(sourceDirectory+string(filesep)+waveformInfoName);
+waveformInfo = f.info;
+
+% Загрузить отсчеты сигнала
+f = load(sourceDirectory+string(filesep)+waveformSourceName);
+waveformSamples = f.txWaveform;
+
+% Создать объект класса analyzer
+analyzer = WaveformAnalyzer(waveformSamples, waveformInfo);
+
+% Рассчитать параметры сигнала
+analyzer.calcWaveformParameters();
+
+% Вывести параметры сигнала
+fprintf("Средняя мощность сигнала: %f\n", analyzer.waveformMeanPower);
+fprintf("Полоса сигнала: %f Hz\n", analyzer.channelBandwidth);
+fprintf("Тип модуляции сигнала: %s\n", analyzer.modulationType);
+fprintf("Длительность сигнала: %f seconds\n", analyzer.waveformDuration);
+
+% Построить спектральную плотность мощности
+analyzer.plotPowerSpectrumDensity();
+
+% Построить созвездие
+analyzer.plotPayloadConstellation();
+
+
+
